1dece8554c7549ea38ba0a76b8e8a8f45eb202ba
israelagoeiro <israelagoeiro@gmail.com>
2021-05-10 06:27:40 -0300

    Commit inicial

---
+++ example/api_connect_example.dart
@@ -0,0 +1,6 @@
+import 'package:api_connect/api_connect.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/api_connect.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library api_connect;
+
+export 'src/api_connect_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
